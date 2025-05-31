0ec544948b948dd005f6ff5c35eb7ef567565eab
kannankannabiran <kannankannabiran@gmail.com>
2025-05-31 11:31:22 +0530

    Parcel package Insall and run by kannan

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-const title = React.createElement('div', {id:'parent'},
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement('div', {id:'parent'},
     React.createElement('div',{id:'child'},
-    React.createElement('h1',{},'Hello World'),
+        React.createElement('h1',{},"Kannabiran!"),
     )
-);
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(title);
+root.render(heading);
