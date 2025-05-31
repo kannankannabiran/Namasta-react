229a383dd185b6b78ecb864e73a56bca49247065
kannankannabiran <kannankannabiran@gmail.com>
2025-05-31 13:04:53 +0530

    igniting app  Lession 2 compleated by kannan

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement('div', {id:'parent'},
     React.createElement('div',{id:'child'},
-        React.createElement('h1',{},"Kannabiran!"),
+        React.createElement('h1',{},"Kannan!"),
     )
 )
 const root = ReactDOM.createRoot(document.getElementById('root'));
